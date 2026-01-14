145efb7071454eecb2c5e45109253c6b8d14cc15
Raúl Martín <55482385+rmartinsanta@users.noreply.github.com>
2026-01-14 11:52:14 +0100

    Enhance Moodle exam review script functionality
    Updated the script to improve UI for grading and fixed light-dark mode text editors.

--- moodle/better-exam-review.js
+++ moodle/better-exam-review.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Better Moodle exam review
 // @namespace   raulmart.in
-// @match       *://*/moodle/mod/quiz/report.php*
+// @match       *://*/moodle/mod/quiz/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Raúl Martín
-// @description Last updated 3/8/2020, 8:42:28 PM
+// @description Fix light-dark mode text editors, improve UI when grading 
 // @run-at		document-end
 // @downloadURL https://raw.githubusercontent.com/rmartinsanta/userscripts/main/moodle/better-exam-review.js
 // ==/UserScript==
@@ -15,24 +15,52 @@ PREFER_TABS = true
 VIEW_URL = "mod/quiz/reviewquestion.php"
 EDIT_URL = "mod/quiz/comment.php"
 
+const FIX_DARK_LIGHT_MODE = `
+.answer div.qtype_essay_response {
+	background-color: white !important;
+}
+
+.answer div.qtype_essay_response, body.urjc-darkmode .answer div.qtype_essay_response span, body.urjc-darkmode .answer div.qtype_essay_response p, body.urjc-darkmode .answer div.qtype_essay_response div {
+	color: black !important;
+}
+
+
+body.urjc-darkmode .answer div.qtype_essay_response {
+	background-color: black !important;
+}
+
+body.urjc-darkmode .answer div.qtype_essay_response, body.urjc-darkmode .answer div.qtype_essay_response span, body.urjc-darkmode .answer div.qtype_essay_response p, body.urjc-darkmode .answer div.qtype_essay_response div {
+	color: white !important;
+}
+`
+
+function addCSS(css){
+	const styleSheet = document.createElement("style");
+	styleSheet.textContent = css;
+	document.head.appendChild(styleSheet);
+}
 
 function fixLink(node) {
     node.href = node.href.replace(VIEW_URL, EDIT_URL);
 }
 
-// replace all links in results page
-document.querySelectorAll(`a[href*='${VIEW_URL}']`).forEach(fixLink);
+addCSS(FIX_DARK_LIGHT_MODE);
 
-// YUI registers hundreds of event handlers to open shitty windows. 
-// Intercept calls to openpopup, check if arg is reviewquestion.php, and replace it. 
-const originalOpenPopup = window.openpopup;
-window.openpopup = function(e, args) {
-        if (args.url.indexOf(VIEW_URL) >= 0) {
-			const url = args.url.replace(VIEW_URL, EDIT_URL);
-			if (PREFER_TABS){
-				if(e.preventDefault) e.preventDefault();
-				return window.open(url, "_blank");
-			}
-        } 
-	return originalOpenPopup(e, args);
-};
+if (window.location.indexOf("moodle/mod/quiz/report.php") > 0){
+	// replace all links in results page
+	document.querySelectorAll(`a[href*='${VIEW_URL}']`).forEach(fixLink);
+	
+	// YUI registers hundreds of event handlers to open shitty windows. 
+	// Intercept calls to openpopup, check if arg is reviewquestion.php, and replace it. 
+	const originalOpenPopup = window.openpopup;
+	window.openpopup = function(e, args) {
+	        if (args.url.indexOf(VIEW_URL) >= 0) {
+				const url = args.url.replace(VIEW_URL, EDIT_URL);
+				if (PREFER_TABS){
+					if(e.preventDefault) e.preventDefault();
+					return window.open(url, "_blank");
+				}
+	        } 
+		return originalOpenPopup(e, args);
+	};
+}
